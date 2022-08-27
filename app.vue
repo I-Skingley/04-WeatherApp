d259831d534bc6f3c480d25357f3219de8e12c1f
I-Skingley <ijskingley@gmail.com>
2022-08-27 16:48:49 +0200

    Hardcoded api fetch

--- app.vue
+++ app.vue
@@ -1,17 +1,25 @@
+<script setup lang="ts">
+//https://api.openweathermap.org/data/2.5/weather?q=Lillehammer&appid=c6bd29d73061465642ea8b1bc459b031
+const {data: city, error} = useFetch("https://api.openweathermap.org/data/2.5/weather?q=Lillehammer&units=metric&appid=c6bd29d73061465642ea8b1bc459b031"); //rename data to city
+
+</script>
+
 <template>
   <div class="h-screen overflow-hidden">
+    {{city}}
+    {{error}}
     <img />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <!--padding 48-->
       <div class="flex justify-between">
         <div>
-          <h1 class="text-7xl text-white">Toronto</h1>
+          <h1 class="text-7xl text-white">{{city.name}}</h1>
           <p class="font-extralight text-2xl text-white">Sunday Dec 9th</p>
-          <img class="w-56 icon" />
+          <img :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`" class="w-56 icon" />>
         </div>
         <div>
-          <p class="text-9xl text-white font-extralight">53 °</p>
+          <p class="text-9xl text-white font-extralight">{{city.main.temp}}°</p>
         </div>
       </div>
       <div class="mt-20 flex">
@@ -25,5 +33,10 @@
 <style scoped>
 .overlay {
   background-color: rgba(0, 0, 0, 0.5);
+}
+
+.icon {
+  margin-left: -2.75rem;
+  margin-top:  -2.75rem;
 }
 </style>

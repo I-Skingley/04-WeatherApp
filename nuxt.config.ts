1a558d525c6f643ab2168a1bd1c431b80882ac8d
I-Skingley <ijskingley@gmail.com>
2022-08-28 12:44:04 +0200

    Runtime Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    publicRuntimeConfig: {  //client and server access
+        APIKEY: process.env.APIKEY, //Would be better as private but would need to run own server
+    },
+    privateRuntimeConfig: { //server access only
+        HELLO: "world in the server not the client",
+    },
     css: ["~/assets/css/tailwind.css"],
     build: {
         postcss: {
